f6833cbe9f39affc754790a8b2167768565293e8
Coen Mooij <coenmooij@gmail.com>
2025-05-30 17:25:56 +0200

    create coming soon page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: 'app.html',
+  styleUrl: 'app.scss',
 })
 export class App {
-  protected title = 'digitalvibe.com';
+  @ViewChild('audioElement') public audioElement!: ElementRef<HTMLAudioElement>;
+
+  public isPlaying: boolean = false;
+
+  private interval?: number;
+
+  public playHeartbeat(): void {
+    if (!this.isPlaying) return;
+
+    this.audioElement.nativeElement.currentTime = 0;
+    this.audioElement.nativeElement.play().then();
+  }
+
+  public onPlaySound(): void {
+    this.isPlaying = !this.isPlaying;
+
+    if (this.isPlaying) {
+      this.playHeartbeat();
+      this.interval = setInterval(() => this.playHeartbeat(), 1600);
+    } else {
+      clearInterval(this.interval);
+    }
+
+  }
 }
